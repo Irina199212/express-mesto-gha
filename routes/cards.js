aa4786d77fb7ce11a7d0c546f7a6cb4953ad72f9
Irina <irinawork1992@yandex.ru>
2023-03-30 22:35:33 +0300

    upd

--- routes/cards.js
+++ routes/cards.js
@@ -14,8 +14,8 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object()
     .keys({
-      name: Joi.string().min(2).max(30),
-      link: Joi.string().pattern(new RegExp(patternurl)),
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().pattern(new RegExp(patternurl)),
     })
     .unknown(true),
 }), createCard);
